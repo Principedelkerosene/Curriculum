c20d99acfb8d43db2e989aa88ee1057bc72bd2a6
Principedelkerosene <90873360+Principedelkerosene@users.noreply.github.com>
2023-02-28 13:35:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+<script>
+<div id="nav-links">
+  <a href="#info" class="boton1">Información Personal</a>
+  <a href="#experiencia" class="boton2">Experiencia</a>
+  <a href="#habilidades" class="boton3">Habilidades</a>
+</div>
+<section id="info">
+</section>
+
+<section id="experiencia">
+</section>
+
+<section id="habilidades">
+</section>
+</script>
